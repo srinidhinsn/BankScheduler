bca5273deda004561b7329e31639e6b0a9966bd1
Srinidhi N <srinidhinsn@gmail.com>
2022-02-09 14:02:12 +0000

    Update QueryConstants.java

--- src/main/java/com/distarise/bank/database/QueryConstants.java
+++ src/main/java/com/distarise/bank/database/QueryConstants.java
@@ -10,15 +10,15 @@ public class QueryConstants {
             " from sb_trans tran, pig_master master where tran.sb_ano = master.pg_acno and " +
             " master.cno = ? and master.pg_dom >= sysdate order by tran.sb_ano, tran.sb_dot desc";
 
-    public static String GET_REM_ACCT = "select fd.fd_acno as sb_ano, fd.fd_prin as sb_bal,t.fd_dot as sb_dot, t.fd_part as sb_part, t.head as head,"+
-    " fd.fd_dor as pg_dom, fd.fd_doa as pg_isd, fd.fd_prin as pg_con, fd.fd_int as pg_int, fd.fd_amt as sb_val from fd_dep_det fd, fd_master m, fd_trans t where"+
-    " m.cno = ? and m.fd_acno=fd.fd_acno and fd.fd_doa <= sysdate and fd.fd_acno=t.fd_acno"+
+    public static String GET_REM_ACCT = "select fd.fd_acno as sb_ano, fd.fd_prin as sb_bal,sysdate as sb_dot, 'Renewal' as sb_part, m.head as head,"+
+    " fd.fd_dor as pg_dom, fd.fd_doa as pg_isd, fd.fd_prin as pg_con, fd.fd_int as pg_int, fd.fd_amt as sb_val from fd_dep_det fd, fd_master m where"+
+    " m.fd_acno=fd.fd_acno and fd.fd_dor >= sysdate and m.fd_stat <> 'CLOSED' and m.cno = ? "+
     " union"+
     " select m.hl_lano as sb_ano,l.sl_bal as sb_bal, l.sl_dot as sb_dot, l.sl_part as sb_part, l.head as head," +
     " sysdate as pg_dom, sysdate as pg_isd, '0' as pg_con, '0' as pg_int, '0' as sb_val " +
-    " from sl_master m, sl_ledger l where m.hl_lano =l.sl_ano and m.cno=?"+
+    " from sl_master m, sl_ledger l where m.hl_lano =l.sl_ano and m.hl_stat <> 'CLOSED' and  m.cno=? "+
     " union"+
     " select tran.sb_ano, tran.sb_bal, tran.sb_dot, tran.sb_part, tran.head, master.pg_dom, master.pg_isd, master.pg_con, master.pg_int, '0' as  sb_val"+
     " from sb_trans tran, pig_master master where tran.sb_ano = master.pg_acno and"+
-    " master.cno = ? and master.pg_dom >= sysdate order by sb_ano, sb_dot desc";
+    " master.pg_dom >= sysdate and master.cno = ? order by sb_ano, sb_dot desc";
 }
